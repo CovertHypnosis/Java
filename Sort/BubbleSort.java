a2176820d106a27175431f6354d1a73f48c4eec8
Miracle <nigifaeg3@gmail.com>
2016-02-05 23:48:21 +0400

    Bubble sort using Generics

--- Sort/BubbleSort.java
+++ Sort/BubbleSort.java
@@ -1,12 +1,31 @@
+import java.util.Comparator;
+
 public class BubbleSort {
-	public static void bubbleSort(int[] list) {
+	public static <E extends Comparable<E>> void bubbleSort(E[] list) {
 		boolean needNextPass = true;
 		
 		for (int k = 1; k < list.length && needNextPass; k++) {
 			needNextPass = false;
 			for (int i = 0; i < list.length - k; i++) {
-				if (list[i] > list[i + 1]) {
-					int temp = list[i];
+				if ((list[i].compareTo(list[i + 1])) < 0) {
+					E temp = list[i];
+					list[i] = list[i + 1];
+					list[i + 1] = temp;
+					
+					needNextPass = true;
+				}
+			}
+		}
+	}
+	
+	public static <E> void bubbleSort (E[] list, Comparator <? super E> comparator) {
+		boolean needNextPass = true;
+		
+		for (int k = 1; k < list.length && needNextPass; k++) {
+			needNextPass = false;
+			for (int i = 0; i < list.length - k; i++) {
+				if ((comparator.compare(list[i], list[i + 1])) < 0) {
+					E temp = list[i];
 					list[i] = list[i + 1];
 					list[i + 1] = temp;
 					
