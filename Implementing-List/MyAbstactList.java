0bc510056f6ad4ecf0d9a63dba48b9a7cf6e2457
Miracle <nigifaeg3@gmail.com>
2016-02-07 03:30:51 +0400

    Create MyAbstactList.java

---
+++ Implementing-List/MyAbstactList.java
@@ -0,0 +1,34 @@
+public abstract class MyAbstactList<E> implements MyList<E> {
+	protected int size = 0;
+	
+	protected MyAbstactList() {}
+	
+	protected MyAbstactList(E[] objects) {
+		for (int i = 0; i < objects.length; i++)
+			add(objects[i]);
+	}
+	
+	@Override
+	public void add(E e) {
+		add(size, e);
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	@Override
+	public int size() {
+		return size;
+	}
+	
+	@Override
+	public boolean remove(E e) {
+		if (indexOf(e) >= 0) {
+			remove(indexOf(e));
+			return true;
+		} else
+			return false;
+	}
+}
