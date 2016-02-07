8d749df86719bf8b9583c53f3cc6a2b24b7c8d1c
Miracle <nigifaeg3@gmail.com>
2016-02-07 22:42:15 +0400

    Create GenericQueue.java

---
+++ Implementing-List/GenericQueue.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+
+public class GenericQueue<E> {
+	private LinkedList<E> list = new LinkedList<>();
+	
+	public void enqueue(E e) {
+		list.addLast(e);
+	}
+	
+	public E dequeue() {
+		return list.removeFirst();
+	}
+	
+	public int getSize() {
+		return list.size();
+	}
+	
+	@Override
+	public String toString() {
+		return "Queue: " + list.toString();
+	}
+}
