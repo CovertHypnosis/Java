824f3d8d91dcf359cdddce83c2ac102b2fc836b5
Miracle <nigifaeg3@gmail.com>
2019-02-12 14:12:52 +0400

    Delete ShipwreckRepository.java

--- das-boot/src/main/java/com/boot/repository/ShipwreckRepository.java
+++
@@ -1,6 +0,0 @@
-package com.boot.repository;
-
-import com.boot.model.Shipwreck;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long> { }
